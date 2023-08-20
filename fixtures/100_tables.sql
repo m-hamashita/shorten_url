a64b8832c02545be963251a1967f9e49acf86692
m-hamashita <masakatsu.hamashita@gunosy.com>
2023-08-20 21:57:46 +0900

    dummy page

--- fixtures/100_tables.sql
+++ fixtures/100_tables.sql
@@ -1,5 +1,13 @@
-CREATE TABLE IF NOT EXISTS `shorten`.`url_mapping` (
+-- データベースへの移動
+USE shorten;
+
+-- url_mappingテーブルの作成
+CREATE TABLE IF NOT EXISTS url_mapping (
     id INT AUTO_INCREMENT PRIMARY KEY,
     short_url VARCHAR(255) NOT NULL,
-    original_url LONGTEXT NOT NULL,
+    original_url VARCHAR(1024) NOT NULL
 );
+
+-- userに対する権限の付与
+GRANT ALL PRIVILEGES ON shorten.* TO 'user'@'%';
+FLUSH PRIVILEGES;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use mysql_async::prelude::*;
+
+async fn shorten(body: warp::hyper::body::Bytes) -> Result<impl warp::Reply, warp::Rejection> {
+    let input_url = String::from_utf8(body.to_vec()).unwrap_or_default();
+    println!("Received URL: {}", input_url);
+
+    let short_url = save_to_mysql(&input_url).await;
+
+    Ok(warp::reply::html(format!(
+        r#"
+        <p>Shortened URL: <a href="{short_url}">{short_url}</a></p>
+        "#,
+        short_url = short_url,
+    )))
+}
+
+async fn save_to_mysql(url: &str) -> String {
+    let database_url = mysql_async::Opts::from_url("mysql://user:password@127.0.0.1:3306/shorten").unwrap();
+    let pool = mysql_async::Pool::new(database_url);
+    let mut conn = pool.get_conn().await.expect("Failed to connect to database.");
+
+    let short_url = "http://short.url/abcd1234".to_string(); // dummy url
+
+    conn.exec_drop(
+        r"INSERT INTO url_mapping (short_url, original_url) VALUES (:short_url, :original_url)",
+        params! {
+            "short_url" => &short_url,
+            "original_url" => &url,
+        },
+    )
+    .await
+    .expect("Failed to insert data.");
+
+    drop(conn);
+
+    short_url
+}
+
+#[tokio::main]
+async fn main() {
+    let index = warp::path::end()
+        .map(|| warp::reply::html(
+            r#"
+            <form action="/shorten" method="post">
+                <input type="text" name="url">
+                <input type="submit" value="Shorten">
+            </form>
+            "#
+        ));
+
+    let shorten_route = warp::path!("shorten")
+        .and(warp::post())
+        .and(warp::body::bytes())
+        .and_then(shorten);
+
+    let routes = index.or(shorten_route);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
